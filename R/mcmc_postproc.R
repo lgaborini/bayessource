10edd0cf6c79451b42ebde2eebf7f21eb4311e6c
Lorenzo Gaborini <lorenzo.gaborini@unil.ch>
2017-07-20 16:32:41 +0200

    mcmc_postproc.R: added cumulative means. Moved back exact values in report_test_wishart_sampling.R.

--- R/mcmc_postproc.R
+++ R/mcmc_postproc.R
@@ -10,16 +10,17 @@ library(coda)
 #' Can be used to post-process outputs from \code{\link{marginalLikelihood}} when \code{output.mcmc} is TRUE.
 #' Notice that it performs matrix inversions on every sample of W.
 #'
-#' It is also possible to compute ML estimators for the posterior means.
+#' It is also possible to compute ML estimators for the posterior means, as well as the cumulated estimates.
 #' The results will be appended to the returned list.
 #'
 #' @param mcmc.output The coda object
 #' @param compute.ML if TRUE, also compute the posterior means
+#' @param cumulative if TRUE, also compute cumulative posterior means to assess precision
 #'
 #' @return a named list with multidimensional arrays for W, theta, and ML estimates
 #' @export
 #'
-mcmc_postproc <- function(mcmc.output, compute.ML = FALSE) {
+mcmc_postproc <- function(mcmc.output, compute.ML = FALSE, cumulative = TRUE) {
    stopifnot(is.mcmc(mcmc.output))
 
 
@@ -39,6 +40,11 @@ mcmc_postproc <- function(mcmc.output, compute.ML = FALSE) {
    if (compute.ML) {
       list.out$W.samples.ML <- apply(W.samples.cube, c(2,3), mean)
       list.out$theta.samples.ML <- as.matrix(colMeans(theta.samples))
+
+      if (cumulative) {
+         list.out$W.samples.ML.cum <- apply(W.samples.cube, c(2,3), cummean)
+         list.out$theta.samples.ML.cum <- apply(as.matrix(theta.samples), 2, cummean)
+      }
    }
    list.out
 }
