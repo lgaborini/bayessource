af0ec19afe8cbc368c32f8730c2515e5999b7b45
Lorenzo Gaborini <lorenzo.gaborini@unil.ch>
2017-10-12 10:02:09 +0200

    Fixed C++11 support: now OpenMP does not complain w/ latest RcppArmadillo.

--- src/config.h
+++ src/config.h
@@ -6,6 +6,9 @@
 // Store constants, package-wide stuff
 //-----------------------------------------------
 
+// Enable C++11 (also look for flags in Makefiles)
+// [[Rcpp::plugins(cpp11)]]
+
 // Enable OpenMP
 // [[Rcpp::plugins(openmp)]]
 #include <omp.h>
